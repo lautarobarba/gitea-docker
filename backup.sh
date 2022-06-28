62d5d2f9f3c15c57fac2ef284a880961f6ff18a0
Administrador <informatica@dposs.gov.ar>
2022-06-28 09:02:29 -0300

    Se actualizo script backup

--- backup.sh
+++ backup.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
 
-LOG_DIR=./backup.log
+SCRIPT_PATH=$(realpath $0)
+WORKDIR=$(dirname ${SCRIPT_PATH})
+LOG_DIR=${WORKDIR}/backup.log
 FECHA=`date +%Y%m%d`
 
 echo [`date`] ========== Comenzando script Backup - GITEA ========== >> $LOG_DIR
 
-echo [`date`] ::Comprimiendo $(pwd)/data a $(pwd)/backups/backupGitea${FECHA}.zip >> $LOG_DIR
-sudo zip -r ./backups/backupGitea${FECHA}.zip data
+echo [`date`] ::Comprimiendo ${WORKDIR}/data a ${WORKDIR}/backups/backupGitea${FECHA}.zip >> $LOG_DIR
+zip -r ${WORKDIR}/backups/backupGitea${FECHA}.zip ${WORKDIR}/data
 echo [`date`] ___ OK >> $LOG_DIR
 echo ------------------------------------ >> $LOG_DIR
-echo [`date`] ::Copiando de $(pwd)/backupGitea${FECHA}.gzip a lautaro@192.168.150.50:/home/lautaro/Backups_150/gitea/gitea_2022 >> $LOG_DIR
-sudo scp ./backups/backupGitea${FECHA}.zip lautaro@192.168.150.50:/home/lautaro/Backups_150/gitea/gitea_2022
+
+echo [`date`] ::Copiando de ${WORKDIR}/backupGitea${FECHA}.gzip a lautaro@192.168.150.50:/home/lautaro/Backups_150/gitea/gitea_2022 >> $LOG_DIR
+scp ${WORKDIR}/backups/backupGitea${FECHA}.zip lautaro@192.168.150.50:/home/lautaro/Backups_150/gitea/gitea_2022
 echo [`date`] ___ OK >> $LOG_DIR
 echo ------------------------------------ >> $LOG_DIR
-echo [`date`] ::Eliminando ./backups/backupGitea${FECHA}.zip >> $LOG_DIR
-sudo rm ./backups/backupGitea${FECHA}.zip
+
+echo [`date`] ::Eliminando ${WORKDIR}/backups/backupGitea${FECHA}.zip >> $LOG_DIR
+rm ${WORKDIR}/backups/backupGitea${FECHA}.zip
 echo [`date`] ___ OK >> $LOG_DIR
 echo ------------------------------------ >> $LOG_DIR
+
 exit 0
+
